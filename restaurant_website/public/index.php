7b9652c5d1b42c490288e68690066b724d43d3be
Dusan Jankovic <dukkelaaj@gmail.com>
2019-11-14 13:45:25 +0100

    I prepare some pages for next actions.

---
+++ restaurant_website/public/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Mexican Restaurant">
+    <meta name="keywords" content="drink, food">
+    <meta name="author" content="Dusan Jankovic">
+    <title>Toca Madera</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="header-notification">
+        <p>Our Scottsdale location is now OPEN | <a href="#">Reservations</a><i class="fa fa-close"></i></p>
+      </div>
+      <nav>
+        <div class="logo">
+          <img src="" alt="logo_img">
+        </div>
+        <div class="header-menu">
+          <ul>
+            <li><a href="">Experience</a></li>
+            <li>
+              <a href="">Menus<i class="fa fa-angle-down"></i></a>
+                 <ul>
+                   <li><a href="">Overview</a></li>
+                   <li><a href="">Scottsdale</a></li>
+                   <li><a href="">Los Angeles Dinner Menu</a></li>
+                   <li><a href="">Vegan Dinner Menu</a></li>
+                   <li><a href="">Dinner Cocktails</a></li>
+                   <li><a href="">Dulces</a></li>
+                   <li><a href="">Happy Hour</a></li>
+                   <li><a href="">Los Angeles Brunch</a></li>
+                   <li><a href="">Los Angeles Brunch Cocktails</a></li>
+                 </ul>
+            </li>
+            <li>
+              <a href="">Events<i class="fa fa-angle-down"></i></a>
+                <ul>
+                  <li><a href="">Our Calendar</a></li>
+                  <li><a href="">Book your Event</a></li>
+                </ul>
+            </li>
+            <li>
+              <a href="">Locations<i class="fa fa-angle-down"></i></a>
+                <ul>
+                  <li><a href="">West Hollywood</a></li>
+                  <li><a href="">Scottsdale</a></li>
+                </ul>
+            </li>
+            <li><a href="">Careers</a></li>
+            <li><a href="" class='reservation-button'>Reservations</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+
+    <main>
+      <div class="slide-show">
+        <div class="slide">
+          <img src="" alt="">
+            <i class="fa fa-circle"></i>
+            <i class="fa fa-angle-down"></i>
+        </div>
+      </div>
+
+      <div class="intro">
+        <h1>Toca Madera is a modern take on traditional Mexican cuisine, featuring unique shared plates and specialty craft cocktails.</h1>
+        <h3>Now Open in Los Angeles & Scottsdale</h3>
+            <a href="" class='reservation-button'>Reservations</a>
+      </div>
+
+      <div class="test-Toca">
+        <h3>TASTE. TOUCH. HEAR. EXPERIENCE.</h3>
+         <span>TOCA MADERA</span>
+           <a href="" class='reservation-button'>View Menus</a>
+            <img src="" alt="">
+      </div>
+
+      <div class="latest">
+        <h3>The Latest</h3>
+           <section>
+             <img src="" alt="">
+                <h4>Phoenix Magazine</h4>
+                  <p>Modern Mexican Restaurant Toca Madera debuts in Scottsdale</p>
+                  <p>View More</p>
+           </section>
+           <section>
+             <img src="" alt="">
+                <h4>Jetsetmag</h4>
+                  <p>Hot Spot: Toca Madera has conqured west Hollywood</p>
+                  <p>View More</p>
+           </section>
+      </div>
+
+      <div class="news-events">
+        <h2>News & Events</h2>
+           <p>What's Happening at Toca Madera</p>
+            <a href="" class='reservation-button'>View More</a>
+      </div>
+
+      <div class="poster">
+        <a href="">
+          <div class="">
+            <img src="" alt="">
+          </div>
+        </a>
+      </div>
+    </main>
+
+
+    <footer>
+      <div class="social-net">
+        <a href="#"><i class="fa fa-facebook-f"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+      </div>
+
+      <div class="find-us">
+        <a href="#">Contact us</a>
+        <a href="#">Know before you go</a>
+        <button type='button' class='reservation-button'>Join our newsletter</button>
+      </div>
+    </footer>
+
+
+  </body>
+</html>
