95d739df4306d16dff4251d01623f756ac4b2244
Dusan Jankovic <dukkelaaj@gmail.com>
2019-11-27 22:57:40 +0100

    Separet one file into 3, and use php include to connect them.

---
+++ restaurant_website/public/footer.php
@@ -0,0 +1,16 @@
+<footer>
+  <div class='mobile-footer'>
+    RESERVATIONS
+  </div>
+  <div class="social-net">
+    <a href="#"><i class="fa fa-facebook-f"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <a href="#"><i class="fa fa-instagram"></i></a>
+  </div>
+
+  <div class="find-us">
+    <a href="#">Contact us</a>
+    <a href="#">Know before you go</a>
+    <button type='button' class='reservation-button'>Join our newsletter</button>
+  </div>
+</footer>
--- restaurant_website/public/index.php
+++ restaurant_website/public/index.php
@@ -1,210 +1,24 @@
+<?php require 'variables.php'; ?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Mexican Restaurant">
-    <meta name="keywords" content="drink, food">
-    <meta name="author" content="Dusan Jankovic">
-    <title>Toca Madera</title>
-    <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/home.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+<html>
+<head>
+<?php include 'meta_tags.php'; ?>
+<title>Toca Madera</title>
+<link rel="stylesheet" href="assets/css/reset.css">
+<link rel="stylesheet" href="assets/css/header.css">
+<link rel="stylesheet" href="assets/css/home.css">
+<link rel="stylesheet" href="assets/css/footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+</head>
 
-  </head>
   <body>
-    <header>
-      <div class="header-notification">
-        <p>Our Scottsdale location is now <span>OPEN</span> | <a href="#">Reservations</a><i id='close' class="fa fa-close"></i></p>
-      </div>
-      <nav>
-        <div class="logo">
-          <img src="assets/images/logo.png" alt="website_logo_image">
-        </div>
-        <div class="header-menu">
-          <ul class='main-ul'>
-            <li><a href="">Experience</a></li>
-            <li class='li-dropdown'>
-               Menus<i class="fa fa-angle-down"></i>
-                 <ul class="sub-ul">
-                   <li><a href="">Overview</a></li>
-                   <li><a href="">Scottsdale Dinner Menu</a></li>
-                   <li><a href="">Los Angeles Dinner Menu</a></li>
-                   <li><a href="">Vegan Dinner Menu</a></li>
-                   <li><a href="">Dinner Cocktails</a></li>
-                   <li><a href="">Dulces</a></li>
-                   <li><a href="">Happy Hour</a></li>
-                   <li><a href="">Los Angeles Brunch</a></li>
-                   <li><a href="">Los Angeles Brunch Cocktails</a></li>
-                 </ul>
-            </li>
-            <li class='li-dropdown'>
-                Events<i class="fa fa-angle-down"></i>
-                <ul class="sub-ul">
-                  <li><a href="">Our Calendar</a></li>
-                  <li><a href="">Book your Event</a></li>
-                </ul>
-            </li>
-            <li class='li-dropdown'>
-              Locations<i class="fa fa-angle-down"></i>
-                <ul class="sub-ul">
-                  <li><a href="">West Hollywood</a></li>
-                  <li><a href="">Scottsdale</a></li>
-                </ul>
-            </li>
-            <li><a href="">Careers</a></li>
-            <li><a href="" class='reservation-button'>Reservations</a></li>
-          </ul>
-        </div>
-
-      <!-- nav bar for small devices - Begin -->
-        <div class="header-menu header-menu2">
-          <div class='wrapper'>
-          <ul class='main-ul'>
-            <li><a href="">Experience</a></li>
-            <li class='li-dropdown'>
-               Menus<i class="fa fa-angle-down"></i>
-                 <ul class="sub-ul">
-                   <li><a href="">Overview</a></li>
-                   <li><a href="">Scottsdale Dinner Menu</a></li>
-                   <li><a href="">Los Angeles Dinner Menu</a></li>
-                   <li><a href="">Vegan Dinner Menu</a></li>
-                   <li><a href="">Dinner Cocktails</a></li>
-                   <li><a href="">Dulces</a></li>
-                   <li><a href="">Happy Hour</a></li>
-                   <li><a href="">Los Angeles Brunch</a></li>
-                   <li><a href="">Los Angeles Brunch Cocktails</a></li>
-                 </ul>
-            </li>
-            <li class='li-dropdown'>
-                Events<i class="fa fa-angle-down"></i>
-                <ul class="sub-ul">
-                  <li><a href="">Our Calendar</a></li>
-                  <li><a href="">Book your Event</a></li>
-                </ul>
-            </li>
-            <li class='li-dropdown'>
-              Locations<i class="fa fa-angle-down"></i>
-                <ul class="sub-ul">
-                  <li><a href="">West Hollywood</a></li>
-                  <li><a href="">Scottsdale</a></li>
-                </ul>
-            </li>
-            <li><a href="">Careers</a></li>
-            <li><a href="">Know before you go</a></li>
-            <li><a href="" >Reservations</a></li>
-            <li><a href="" >Join our newsletter</a></li>
-          </ul>
-
-          <ul class="social-net-ul">
-            <a href="#"><i class="fa fa-facebook-f"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-          </ul>
-
-          <div class="website-author">
-            <p>Powered by Dusan Jankovic</p>
-          </div>
-        </div>
-      </div>
-        <div class='menu-icon'>
-          <div class="top"></div>
-          <div class="mid"></div>
-          <div class="bot"></div>
-        </div>
-      </nav>
-      <!-- nav bar for small devices - End -->
-      <div class='header-hack' style='padding:40px;'></div>
-    </header>
-
-    <main>
-      <div class="slide-show">
-          <div class='dot-container'>
-          </div>
-        <a href="#intro">
-          <div class='arrow-container'>
-            <i class="fa fa-angle-down"></i>
-          </div>
-        </a>
-      </div>
-
-
-
-      <div class="intro" id="intro">
-        <h1>Toca Madera is a modern take on traditional Mexican cuisine, featuring unique shared plates and specialty craft cocktails.</h1>
-        <h3>Now Open in Los Angeles & Scottsdale</h3>
-            <a href="" class='reservation-button'>Reservations</a>
-      </div>
-
-      <div class="test-Toca">
-        <div>
-          <h3>TASTE. TOUCH. HEAR. EXPERIENCE.</h3>
-           <span>TOCA MADERA</span>
-             <a href="" class='reservation-button'>View Menus</a>
-         </div>
-      </div>
-
-      <div class="latest">
-        <h3>The Latest</h3>
-          <a href="" class="section-a1">
-             <section>
-               <div>
-                 <img src="assets/images/latest1.jpg" alt="">
-               </div>
-                  <h4>Phoenix Magazine</h4>
-                    <p>Modern Mexican Restaurant Toca Madera debuts in Scottsdale</p>
-                    <p class='view-more'>View More</p>
-             </section>
-           </a>
-           <a href="" class="section-a2">
-             <section>
-               <div>
-                 <img src="assets/images/latest2.jpg" alt="">
-               </div>
-                  <h4>Jetsetmag</h4>
-                    <p>Hot Spot: Toca Madera has conqured west Hollywood</p>
-                    <p class="view-more">View More</p>
-             </section>
-           </a>
-      </div>
-
-      <div class="news-events">
-       <div>
-        <h3>News & Events</h2>
-           <span>What's Happening at Toca Madera</span>
-            <a href="" class='reservation-button'>View More</a>
-        </div>
-      </div>
-
-      <div class="poster">
-
-      </div>
-    </main>
-
-
-    <footer>
-      <div class='mobile-footer'>
-        RESERVATIONS
-      </div>
-      <div class="social-net">
-        <a href="#"><i class="fa fa-facebook-f"></i></a>
-        <a href="#"><i class="fa fa-twitter"></i></a>
-        <a href="#"><i class="fa fa-instagram"></i></a>
-      </div>
-
-      <div class="find-us">
-        <a href="#">Contact us</a>
-        <a href="#">Know before you go</a>
-        <button type='button' class='reservation-button'>Join our newsletter</button>
-      </div>
-    </footer>
-
+    <?php include 'header.php'; ?>
+    <?php include 'page/home.php'; ?>
+    <?php include 'footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
     <script src='assets/js/header.js'></script>
     <script src='assets/js/home.js'></script>
-
   </body>
+
 </html>
