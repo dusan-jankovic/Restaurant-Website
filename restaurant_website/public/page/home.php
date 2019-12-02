526b9dbdd4dfcbc7dbacd1442f5c5f0f56a755c2
Dusan Jankovic <dukkelaaj@gmail.com>
2019-12-02 10:23:19 +0100

    Made home.php using content from index.php

---
+++ restaurant_website/public/page/home.php
@@ -0,0 +1,64 @@
+
+<main>
+  <div class="slide-show">
+      <div class='dot-container'>
+      </div>
+    <a href="#intro">
+      <div class='arrow-container'>
+        <i class="fa fa-angle-down"></i>
+      </div>
+    </a>
+  </div>
+
+
+
+  <div class="intro" id="intro">
+    <h1>Toca Madera is a modern take on traditional Mexican cuisine, featuring unique shared plates and specialty craft cocktails.</h1>
+    <h3>Now Open in Los Angeles & Scottsdale</h3>
+        <a href="" class='reservation-button'>Reservations</a>
+  </div>
+
+  <div class="test-Toca">
+    <div>
+      <h3>TASTE. TOUCH. HEAR. EXPERIENCE.</h3>
+       <span>TOCA MADERA</span>
+         <a href="" class='reservation-button'>View Menus</a>
+     </div>
+  </div>
+
+  <div class="latest">
+    <h3>The Latest</h3>
+      <a href="" class="section-a1">
+         <section>
+           <div>
+             <img src="assets/images/latest1.jpg" alt="">
+           </div>
+              <h4>Phoenix Magazine</h4>
+                <p>Modern Mexican Restaurant Toca Madera debuts in Scottsdale</p>
+                <p class='view-more'>View More</p>
+         </section>
+       </a>
+       <a href="" class="section-a2">
+         <section>
+           <div>
+             <img src="assets/images/latest2.jpg" alt="">
+           </div>
+              <h4>Jetsetmag</h4>
+                <p>Hot Spot: Toca Madera has conqured west Hollywood</p>
+                <p class="view-more">View More</p>
+         </section>
+       </a>
+  </div>
+
+  <div class="news-events">
+   <div>
+    <h3>News & Events</h2>
+       <span>What's Happening at Toca Madera</span>
+        <a href="" class='reservation-button'>View More</a>
+    </div>
+  </div>
+
+  <div class="poster">
+
+  </div>
+</main>
