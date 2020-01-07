b77b651a8698bac14dbff3bac86618501235fef3
Dusan Jankovic <dukkelaaj@gmail.com>
2020-01-07 23:31:32 +0100

    Php file for our events...

--- restaurant_website/public/page/events/our_events.php
+++ restaurant_website/public/page/events/our_events.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
   <html>
   <head>
     <?php include '../../meta_tags.php'; ?>
@@ -31,79 +31,53 @@
       <div class='options'>
         <div class='search-div'>
           <div>
-            <input placeholder='search' type='text' name='search'>
-              <i class="fa fa-close"></i>
           </div>
           <div>
-            <i class="fa fa-angle-left"></i>
-              <input type='date' name='date'>
-            <i class="fa fa-angle-right"></i>
           </div>
         </div>
 
         <div class='events-view'>
-          <span>Subscribe</span>
+          <span id='subscribe'>Subscribe</span>
            <div>
-              <button type='button'>Pinboard</button>
-              <button type="button">Agenda</button>
-              <button type="button">Monthly</button>
+              <button type='button' id='pinb'>Pinboard</button>
+              <button type="button" id='agen'>Agenda</button>
+              <button type="button" id='month'>Monthly</button>
            </div>
          </div>
        </div>
 
-       <h1>Events for 5 jun</h1>
-        <?php
-        //get db connection script
-          include '../../dbconnect.php';
-
-          //make data and push them to the array, and from that array insert them to database (table)
-     // ------------------------------------------------------------------------------------------------  //
-          //number of arrays in array depence on number of columns in table; each array is column with values
-          $data = [
-          //event_name
-            [],
-          //day_date_time
-            [],
-         //images
-            []
-          ];
-
-          $img = 'https://d3flpus5evl89n.cloudfront.net/5ae7a89edf82fe0acf63df87/5bad4c95df82fe2b07c6cb5e/square_272x272.jpg';
-          $img2 = 'https://d3flpus5evl89n.cloudfront.net/5ae7a89edf82fe0acf63df87/5b88e341df82fe27b390eb8d/square_272x272.jpg';
-          $img3 = 'https://d3flpus5evl89n.cloudfront.net/5ae7a89edf82fe0acf63df87/5d31f56adf82fe23aacf574f/square_272x272.jpg';
-
-          for($i = 0; $i < 35; $i++) {
-             array_push($data[0], )
-          }
-
-          //select data from db
-            $sql = "SELECT id, event_name, day_date_time, image FROM our_events";
-            $result = $conn->query($sql);
-
-            //show data from db to the page
-            if ($result->num_rows > 0) {
-
-                // output data of each row
-                while($row = mysqli_fetch_assoc($result)) {
-                  echo "<div class='event'>
-                          <img src=".$row["image"]." alt=''>
-                            <h2>".$row["event_name"]."</h2>
-                              <p>".$row["day_date_time"]."</p>
-                         </div>";
-              }
-            }
-
-            else {
-                echo "0 results";
-            }
-            $conn->close()
-        ?>
-
+      <div class='showMonth'>
+        <i class="fa fa-angle-left"></i>
+          <h1 class='day-month'></h1>
+        <i class="fa fa-angle-right"></i>
       </div>
 
-      </main>
+        <ul class='daysInWeek'>
+          <li>Sunday</li>
+          <li>Monday</li>
+          <li>Tuesday</li>
+          <li>Wednesday</li>
+          <li>Thursday</li>
+          <li>Friday</li>
+          <li>Saturday</li>
+        </ul>
+        <div class='calendar'>
+        </div>
 
-
+        <div class='details-div'>
+          <ul>
+            <li id='back-btn'>back</li>
+              <li><i class="fa fa-twitter"></i></li>
+              <li><i class="fa fa-facebook"></i></li>
+              <li><i class="fa fa-linkedin"></i></li>
+              <li><i class="fa fa-save"></i></li>
+            </ul>
+        </div>
+        <div class='gif'>
+          <img src='http://eliot.dystar.com/eliot/main/images/waiting.gif' alt='load-gif'>
+        </div>
+    </div>
+  </main>
 
    <?php include '../../footer.php'; ?>
 
@@ -113,6 +87,8 @@
       </script>
       <script src='../assets/js/header.js'></script>
       <script src='../assets/js/experience.js'></script>
+      <script src='../assets/js/ourEvents.js'></script>
+
 
     </body>
     </html>
