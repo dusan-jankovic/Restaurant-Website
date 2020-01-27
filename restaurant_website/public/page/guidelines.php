24a3f11003250f04f773dae0c05269ac638a99ae
Dusan Jankovic <dukkelaaj@gmail.com>
2020-01-27 23:32:13 +0100

    Made guidelines page.

---
+++ restaurant_website/public/page/guidelines.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+   <?php include '../meta_tags.php'; ?>
+   <link rel="stylesheet" href="assets/css/reset.css">
+   <link rel="stylesheet" href="assets/css/header.css">
+   <link rel="stylesheet" href="assets/css/page/guidelines.css">
+   <link rel="stylesheet" href="assets/css/footer.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+ </head>
+ <body>
+
+   <?php  $deepFile = 0; ?>
+   <?php include '../header.php'; ?>
+
+ <main>
+   <div class="slide-show">
+       <div class='dot-container'>
+       </div>
+     <a href="#concept">
+       <div class='arrow-container'>
+         <i class="fa fa-angle-down"></i>
+       </div>
+     </a>
+   </div>
+
+  <h1 class='title'>KNOW BEFORE YOU GO</h1>
+   <h4 class='sub-title'>Know Before You Go Guidelines</h4>
+    <p>We invite you to join us for an intimate dining experience in a comfortable, upscale environment.</p>
+    <p>In an effort to create an elevated experience for all of our guests, dining guidelines and dress code are enforced.</p>
+      <h4 class='sub-title'>DINING GUIDELINES</p>
+        <p>- No outside food or beverage is permitted in the restaurant without prior authorization.</p>
+        <p>- While we adhere to the local and state policies regarding service animals, we request that all animals remain on the ground.</p>
+        <p>- We don’t recommend our restaurants for young children and request that you bring a child seat if one is required.</p>
+        <p>- If a significant amount of time has passed, we may ask to accommodate your party at the bar out of consideration for other guests who are on the waiting list.
+          We typically will not make this request less than two hours after you have been checked in for your reservation.</p>
+        <p>- We request that you notify your server of any allergies. Not all ingredients are listed on the menu and we cannot always guarantee there will be no cross-contamination.</p>
+            <h4 class='sub-title'>DRESS CODE</h4>
+              <p>We courteously ask that our guests abide by our dress code policy while dining at Toca Madera. The following clothing items are not permitted:</p>
+              <p>- Beachwear</p>
+              <p>- Athletic wear (including Tank Tops)</p>
+              <p>- Basketball/Gym Shorts</p>
+              <p>- Flip Flops, Rubber & Plastic Thong Sandals</p>
+              <p>Admittance into Toca Madera is at the discretion of the Doorman & Management.
+                We reserve the right to refuse service to patrons who are inappropriately dressed, under the influence,
+                 and/or are wearing articles of clothing containing vulgarity or language that guests may find offensive. Thank you in advance for your cooperation.</p>
+
+</main>
+<script>
+  var filePath = ''
+  var sliderSize = 'sm'
+</script>
+<script src='assets/js/header.js'></script>
+<script src='assets/js/experience.js'></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+ <?php include '../footer.php'; ?>
+</body>
+</html>
