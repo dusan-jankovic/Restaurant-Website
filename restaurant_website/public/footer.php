199714f29a36dd4b902cf0285d729f1098649eaa
Dusan Jankovic <dukkelaaj@gmail.com>
2020-02-04 23:39:46 +0100

    Added footer from for email subsribe.

--- restaurant_website/public/footer.php
+++ restaurant_website/public/footer.php
@@ -1,4 +1,29 @@
 <footer>
+  <div class='email-signup'>
+    <form class='form' method="POST">
+      <h2>Email Signup</h2>
+      <div class='typeField'>
+         <input id='fname' type="text" placeholder="First Name" name='fname' value="<?php echo htmlspecialchars($first_name); ?>">
+          <p class="input-errors"></p>
+       </div>
+
+       <div class='typeField'>
+          <input id='lname' type="text" placeholder="Last Name" name='lname' value="<?php echo htmlspecialchars($lname); ?>">
+            <p class="input-errors"></p>
+        </div>
+
+      <div class='typeField'>
+         <input id='email' placeholder="Email" type="text" name='email' value="<?php echo htmlspecialchars($email); ?>">
+           <p class="input-errors"></p>
+            <button type='submit' name='submit' id='submitBtn'>Submit</button>
+       </div>
+
+       <div class='close-icon cl-i'>
+         <i class='fa fa-close'></i>
+       </div>
+     </form>
+   </div>
+
   <div class='mobile-footer'>
     RESERVATIONS
   </div>
@@ -9,8 +34,13 @@
   </div>
 
   <div class="find-us">
-    <a href="#">Contact us</a>
+    <a href="#"></a>
     <a href="#">Know before you go</a>
-    <button type='button' class='reservation-button'>Join our newsletter</button>
+    <button type='button' class='reservation-button join'>Join our newsletter</button>
   </div>
+
+  <script src='assets/js/footer.js'></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src='assets/js/ajax-newsletter.js'></script>
+
 </footer>
